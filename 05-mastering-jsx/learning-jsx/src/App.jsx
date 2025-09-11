8920b651edbd39ed6ba58a6542e096d3db56b128
MosfiqurRubel <rubel.airin@gmail.com>
2025-09-11 19:04:17 +0600

    (feat) 05 jsx

---
+++ 05-mastering-jsx/learning-jsx/src/App.jsx
@@ -0,0 +1,12 @@
+import { SquarePlay } from "lucide-react";
+import Avatar from "./components/Avatar";
+
+function App() {
+  return (
+    <>
+      <Avatar />
+    </>
+  );
+}
+
+export default App;
